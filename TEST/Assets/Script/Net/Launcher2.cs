6ccae5a1a363e4752a41bce3962a4ba8fce51a3c
foiscs <qkrqntjd12394@gmail.com>
2018-10-24 20:19:23 +0900

    q
    awa

---
+++ TEST/Assets/Script/Net/Launcher2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Launcher2 : Photon.PunBehaviour
+{
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        PlayerPrefs.DeleteAll();
+        PhotonNetwork.ConnectUsingSettings("1");
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+}
